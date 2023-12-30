81e412cb1bec51f5fd523fe142eaddc5e343ceb5
Sherry <128379072+resherry2020@users.noreply.github.com>
2023-12-30 17:01:58 +1030

    finish

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -5,46 +5,6 @@ function About() {
     <div>
       <section class="py-5">
         <div class="container">
-          <div class="row align-items-center gx-4">
-            <div class="col-md-5">
-              <div class="ms-md-2 ms-lg-5">
-                <img class="img-fluid rounded-3" src="Icon/Sherry.png" alt="" />
-              </div>
-            </div>
-            <div class="col-md-6 offset-md-1">
-              <div class="ms-md-2 ms-lg-5">
-                <span class="text-muted">About me</span>
-                <h2 class="display-5 fw-bold">Sherry Yang</h2>
-                <p class="lead">
-                  Hello! I'm a cybersecurity graduate student at the University
-                  of Adelaide, passionate about both the Australian sunshine and
-                  the ocean. Discovering the potential harm caused by sunscreens
-                  with unsafe ingredients to marine life motivated me to create
-                  this platform.
-                </p>
-                <p class="lead mb-0">
-                  As an everyday consumer, I understand the challenge of
-                  evaluating the safety of sunscreen ingredients. That's why I
-                  developed this website – to assist individuals like me, who
-                  care about marine ecosystems and love soaking up the sun by
-                  the sea. Now, you can easily make informed decisions about
-                  sunscreen purchases before enjoying the sunshine responsibly.
-                </p>
-              </div>
-              <div class="d-flex justify-content-end">
-                <a
-                  class="btn btn-success mt-5"
-                  href="https://www.linkedin.com/in/sherry-shen-382184261/"
-                >
-                  Contact me
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section class="py-5">
-        <div class="container">
           <div class="row justify-content-center">
             <div class="col-md-12">
               <div class=" ">
@@ -82,6 +42,46 @@ function About() {
                   offer excellent protection in and out of the water! Our
                   favourite plastic-free packaged, reef-safe sunscreens are;
                 </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="py-5">
+        <div class="container">
+          <div class="row align-items-center gx-4">
+            <div class="col-md-5">
+              <div class="ms-md-2 ms-lg-5">
+                <img class="img-fluid rounded-3" src="Icon/Sherry.png" alt="" />
+              </div>
+            </div>
+            <div class="col-md-6 offset-md-1">
+              <div class="ms-md-2 ms-lg-5">
+                <span class="text-muted">About me</span>
+                <h2 class="display-5 fw-bold">Sherry Yang</h2>
+                <p class="lead">
+                  Hello! I'm a cybersecurity graduate student at the University
+                  of Adelaide, passionate about both the Australian sunshine and
+                  the ocean. Discovering the potential harm caused by sunscreens
+                  with unsafe ingredients to marine life motivated me to create
+                  this platform.
+                </p>
+                <p class="lead mb-0">
+                  As an everyday consumer, I understand the challenge of
+                  evaluating the safety of sunscreen ingredients. That's why I
+                  developed this website – to assist individuals like me, who
+                  care about marine ecosystems and love soaking up the sun by
+                  the sea. Now, you can easily make informed decisions about
+                  sunscreen purchases before enjoying the sunshine responsibly.
+                </p>
+              </div>
+              <div class="d-flex justify-content-end">
+                <a
+                  class="btn btn-success mt-5"
+                  href="https://www.linkedin.com/in/sherry-shen-382184261/"
+                >
+                  Contact me
+                </a>
               </div>
             </div>
           </div>
